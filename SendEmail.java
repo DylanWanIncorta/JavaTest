ffd652b280141645cbb9d7bb87101b00e4d36b69
Dylan Wan <dylan.wan@gmail.com>
2016-02-22 19:14:08 -0800

    Added SendEmail

---
+++ SendEmail.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.PrintWriter;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+public class SendEmail
+{
+   public static void main(String [] args)
+   {    
+      // Recipient's email ID needs to be mentioned.
+      String to = "dylan.wan@incorta.com";
+
+      // Sender's email ID needs to be mentioned
+      String from = "incorta@hostname.net";
+
+      // Assuming you are sending email from localhost
+      String host = "localhost";
+
+      // Get system properties
+      Properties properties = System.getProperties();
+
+      // Setup mail server
+      properties.setProperty("mail.smtp.host", host);
+
+      // Get the default Session object.
+      Session session = Session.getDefaultInstance(properties);
+	  
+	  PrintWriter writer = new PrintWriter(System.out);
+      properties.list(writer);
+	  writer.flush();
+	  
+      try{
+         // Create a default MimeMessage object.
+         MimeMessage message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+         // Set Subject: header field
+         message.setSubject("This is the Subject Line!");
+
+         // Now set the actual message
+         message.setText("This is actual message" + properties);
+
+         // Send message
+         Transport.send(message);
+         System.out.println("Sent message successfully....");
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+	  
+   }
+}
